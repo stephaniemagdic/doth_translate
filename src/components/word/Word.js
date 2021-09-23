90341a3d7696c303beb0f47bd00971a7cb2beeff
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-23 13:38:37 -0600

    Add tooltip import and jsx elements to render

--- src/components/word/Word.js
+++ src/components/word/Word.js
@@ -1,8 +1,13 @@
 import {useState, useEffect} from 'react';
 import {fetchDefinition} from '../../util.js'
+import ReactTooltip from 'react-tooltip';
+import './Word.css'
 
 const Word = ({word}) => {
   const [definition, setDefinition] = useState('')
+  const [wordComponent, setWordComponent] = useState('')
+  //this will be used for styling purposes.
+  const [hasDefinition, setHasDefinition] = useState(false)
 
   const getDefinition = async () => {
     try {
@@ -15,18 +20,40 @@ const Word = ({word}) => {
     }
   }
   
-  useEffect(()=> {
-    if(word.length >= 5 ) {
-    getDefinition()
+
+
+  useEffect(async ()=> {
+    console.log("I am in useEFFECT!")
+    if (word.length >= 5 ) {
+      console.log("I am in conditional")
+    await getDefinition();
+    setHasDefinition(true);
+    //to do.. .if definition is undefined ... 
+    //  //TO DO: // if(definition ! == undefined)
+    //edgecase that we have an empty tooltip.
+    setWordComponent(
+      <>
+       <a data-tip data-for={word}> {word}: {definition}</a>
+      <ReactTooltip id={word} >
+        <span>{definition}</span>
+      </ReactTooltip>
+      </>
+    )
+    
+    } else {
+      setWordComponent(<span>{word}</span>)
     }
   }, [])
 
+  console.log("wordCompoent", wordComponent)
+
   return (
     <div className="Word">
-      <p>{word}</p>
-      <p>{definition}</p>
+     {wordComponent}
     </div>
   )
 }
 
 export default Word;
+
+//to do.. .if definition is undefined ... 
