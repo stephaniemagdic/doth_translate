4b3980fbc83be3e2c88e78300015d62490c38788
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-22 18:42:14 -0600

    Add use effect to getDefinition of word prop

--- src/components/word/Word.js
+++ src/components/word/Word.js
@@ -1,14 +1,31 @@
 import {useState, useEffect} from 'react';
+import {fetchDefinition} from '../../util.js'
 
 const Word = ({word}) => {
   const [definition, setDefinition] = useState('')
 
+  const getDefinition = async () => {
+    try {
+      const data = await fetchDefinition(word)
+      console.log('data---->', data)
+      setDefinition(data[0].meanings[0].definitions[0].definition)
+    } catch (err) {
+      // setError(err)
+      console.log(err)
+    }
+  }
+  
   useEffect(()=> {
-
+    if(word.length >= 5 ) {
+    getDefinition()
+    }
   }, [])
 
   return (
-    <p>{word}</p>
+    <div className="Word">
+      <p>{word}</p>
+      <p>{definition}</p>
+    </div>
   )
 }
 
