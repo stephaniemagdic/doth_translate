e616464b8bf8bc69a1a3dfa5dc484a537735600c
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 12:56:23 -0600

    Create user flow specs

---
+++ cypress/integration/interpretationDisplay_spec.js
@@ -0,0 +1,14 @@
+describe('Interpretation Display User Flows', () => {
+  it('A user should see a quote randomly generated on page load', () => {
+    cy.fetchRandomQuote()
+   
+  });
+
+  it('', () => {
+   
+   
+  });
+
+  // a user should be able to click a button to see a randomly generated quote.
+
+})
