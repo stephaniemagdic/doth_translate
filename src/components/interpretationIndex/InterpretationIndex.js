2de28faea9e4d922a06051826b4be044927eb49b
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 15:49:30 -0600

    Add paths to links

--- src/components/interpretationIndex/InterpretationIndex.js
+++ src/components/interpretationIndex/InterpretationIndex.js
@@ -1,21 +1,7 @@
-// TO DO: this component will be rendered by pulling the value of the choice key from the match param
+// TO DO: this component will be rendered by pulling the value of the choice key from the match param **
 // it will then take that value and do a conditionally pull for one of two fetches based on another property: displayType={"theme"} or displayType={"title"}
 
 // Setup: Take that value and do a fetch depending on the type
-
-
-//Interpretation Index's state
-  // currentQuote
-
-//child components:
- // button - get a new quote, choose a newCategory, choose a new topic, go to my interpretations
-
- //Quote Component -- holds the quote
-   // holds sentenceDefinitions 
-   // will create individual Word Components for each word that has an event listener to pop up the react tooltip.
- 
-  //TextBoxComponent
-  // Submit Finished Interpretation Button
 
   // const InterpretationIndex = ({choice, displayType}) => {
 
@@ -25,23 +11,18 @@
   import './InterpretationIndex.css'
   import {Link} from 'react-router-dom';
 
-  //if in edit mode .. will display interpretation index editMode.
   const InterpretationIndex = ({addInterpretation, addToFavorites}) => {
     const [quote, setQuote] = useState('')
     const [error, setError] = useState(null)
     const [currentInterpretation, setCurrentInterpretation] = useState('')
     const [isDisabled, setIsDisabled] = useState(true)
 
-
     const fetchSingleQuote = async () => {
-      console.log("I am in fetchSingleQuote!")
       try {
         const data = await fetchRandomQuote()
-        console.log('data---->', data)
         setQuote(data.quote.quote)
       } catch (err) {
         setError(err)
-        console.log(err)
       }
     }
   
@@ -75,7 +56,7 @@
           </button>
           </Link>
         </nav>
-        <p>INTERPRETATION INDEX</p>
+        <p>TEST -- INTERPRETATION INDEX</p>
         <p>TEST -- {quote}</p>
         <Quote quote={quote} addToFavorites={addToFavorites} />
         <input
