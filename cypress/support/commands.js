aea0a498dba58f957a4749ce43b79f33d05381eb
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 14:48:09 -0600

    Install Cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,25 +12,33 @@ Cypress.Commands.add('fetchRandomQuote', () => {
 })
 
 
-Cypress.Commands.add('fetchQuoteByTitle', () => {
-  cy.intercept('GET', 'https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/by-play/othello', {
-    statusCode: 200,
-    body: { quote: {
-      quote: 
-        'Which thing to do, if this poor trash of Venice, whom trace for his quick hunting, stand the putting on, i\'ll have our Michael Cassio on the hip, abuse him to the Moor in the rank garb (For fear Cassio with my nightcap too), make the Moor thank me, love me, and reward me for making him egregiously an ass and practicing upon his peace and quiet even to madness.'
-    } 
-    }
-  })
+// Cypress.Commands.add('fetchQuoteByTitle', () => {
+//   cy.intercept('GET', 'https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/by-play/othello', {
+//     statusCode: 200,
+//     body: { quote: {
+//       quote: 
+//         'Which thing to do, if this poor trash of Venice, whom trace for his quick hunting, stand the putting on, i\'ll have our Michael Cassio on the hip, abuse him to the Moor in the rank garb (For fear Cassio with my nightcap too), make the Moor thank me, love me, and reward me for making him egregiously an ass and practicing upon his peace and quiet even to madness.'
+//     } 
+//     }
+//   })
+// })
+
+// Cypress.Commands.add('fetchQuoteByTheme', () => {
+//   cy.intercept('GET', 'https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/by-theme/love', {
+//     statusCode: 200,
+//     body: { quote: {
+//       quote: 'Do protest never loved myself till now infixed beheld myself drawn in the flattering table of her eye.'
+//     }
+//     }
+//   })
+// })
+
+Cypress.Commands.add('addInterpretation', () => {
+  cy.visit('http://localhost:3002/category/theme/love')
+  cy.get('input').type('HERE IS MY INTERPRETATION')
+  cy.get('.submit-btn').click()
 })
 
-Cypress.Commands.add('fetchQuoteByTheme', () => {
-  cy.intercept('GET', 'https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/by-theme/love', {
-    statusCode: 200,
-    body: { quote: {
-      quote: 'Do protest never loved myself till now infixed beheld myself drawn in the flattering table of her eye.'
-    }
-   
-    }
-  })
-})
+
+// cy.get('favorite-btn').click()
 
