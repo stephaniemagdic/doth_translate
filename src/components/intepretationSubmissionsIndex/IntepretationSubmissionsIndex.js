d848cefa14b989cf6e8986dbce0024579bab0aeb
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-23 20:53:08 -0600

    Create functional component skeleton

---
+++ src/components/intepretationSubmissionsIndex/IntepretationSubmissionsIndex.js
@@ -0,0 +1,7 @@
+const InterpretationSubmissionIndex = ({favorites, userSavedInterpretations}) => {
+  return (
+    <p>Interpretation Submissions</p>
+  )
+}
+
+export default InterpretationSubmissionIndex;
