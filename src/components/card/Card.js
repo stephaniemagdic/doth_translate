8f207266b6190c8272dc306352fb463bb6a74d97
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 16:35:17 -0600

    Add conditional for theme and title buttons

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -16,6 +16,16 @@ const Card = ({cardData, type}) => {
       {cardData}
     <button>EDIT</button>
     </div>)
+  } else if (type === 'theme-options') {
+    componentToRender = ( <div className="theme-options">
+      {cardData}
+    <button>EDIT</button>
+    </div>)
+  } else if (type === 'title-options') {
+    componentToRender = ( <div className="title-options">
+    {cardData}
+  <button>EDIT</button>
+  </div>)
   }
 
   return (
