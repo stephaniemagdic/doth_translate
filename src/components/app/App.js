859a96f4eb9f1113a9a4d3c995bda80683130d9d
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-23 20:37:49 -0600

    Create addToFavorites method

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,10 +6,16 @@ import { useState } from 'react';
 
 function App() {
   const [userSavedInterpretations, setUserSavedInterpretations] = useState([])
+  const [favorites, setFavorites] = useState([])
   //Right now we will have our saved interpretations held in app so it can be passed to our my-interpretationIndex
 
   const addInterpretation = (newInterpretation) => {
     setUserSavedInterpretations([...userSavedInterpretations, newInterpretation])
+  }
+
+  const addToFavorites = (newFavorite) => {
+    // TO DO: only add unique values to favorites.
+    setFavorites([...favorites, newFavorite])
   }
 
 
@@ -27,7 +33,7 @@ function App() {
       <Route
       exact path= "/category/theme/:choice"
       // render={(match) =><InterpretationIndex choice={match.params.choice} displayType={"theme"} isEditing={false}/>}
-      render={() => <InterpretationIndex addInterpretation={addInterpretation}/>}
+      render={() => <InterpretationIndex addInterpretation={addInterpretation} addToFavorites={addToFavorites}/>}
       />
       {/* <Route
       exact path= "/category/title/:choice"
