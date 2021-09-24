817b0eeaf77425026c34786aa3fe69b866d5633f
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 15:48:11 -0600

    Create functional skelaton for component

---
+++ src/components/dashboardIndex/DashboardIndex.js
@@ -0,0 +1,23 @@
+//Welcome page
+//choose a quote category
+//two buttons .. with nav links.
+
+import { Link } from 'react-router-dom';
+
+const DashboardIndex = () => {
+  return (
+    <div className="DashboardIndex">
+      <h1>Welcome Lambkin!</h1>
+      <h2>Choose a Quote Category</h2>
+      <Link to=''>
+      <button className="theme-btn">THEME</button>
+      </Link>
+      <Link to=''>
+      <button className="title-btn">Title</button>
+      </Link>
+      
+    </div>
+  )
+}
+
+export default DashboardIndex;
