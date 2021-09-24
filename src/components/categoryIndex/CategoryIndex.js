9e88ff80293b2363ca6fcb76cc68cdd66eb0624a
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 16:12:53 -0600

    Add fetchTitles method to be called in useEffect

--- src/components/categoryIndex/CategoryIndex.js
+++ src/components/categoryIndex/CategoryIndex.js
@@ -1,14 +1,31 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import {fetchAllTitles} from '../../util.js'
 
 export const CategoryIndex = ({category}) => {
   console.log("match-->", category.match.params.type)
+  const [allTitles, setAllTitles] = useState([])
+
+  const fetchTitles = async () => {
+    try {
+      const data = await fetchAllTitles()
+      setAllTitles(data.quote)
+      console.log("data in fetchAllTitles", data)
+    } catch (err) {
+      console.log(err)
+      // setError(err)
+    }
+  }
+
+  //Next Step.. pass data to CardsContainer...
 
   useEffect(() => {
     const fetchType = category.match.params.type
     if(fetchType === 'theme' ) {
-
+      fetchTitles()
+      
     } else if (fetchType === 'title') {
-
+      //TO DO: COMPLETE
+      return
     }
   }, [])
 
