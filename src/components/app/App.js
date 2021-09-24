8eae8d52649b0a0a5a8afb14b6b84d4fbedbcf8d
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-23 19:41:08 -0600

    Add method to save an interpretation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import InterpretationIndex from '../interpretationIndex/InterpretationIndex.js'
+import InterpretationIndex from '../interpretationIndex/InterpretationIndex.js';
+import { useState } from 'react';
+
 
 function App() {
+  const [userSavedInterpretations, setUserSavedInterpretations] = useState([])
+  //Right now we will have our saved interpretations held in app so it can be passed to our my-interpretationIndex
+
+  const addInterpretation = (newInterpretation) => {
+    setUserSavedInterpretations([...userSavedInterpretations, newInterpretation])
+  }
+
+
   return (
     <div className="App">
       <Switch>
@@ -17,7 +27,7 @@ function App() {
       <Route
       exact path= "/category/theme/:choice"
       // render={(match) =><InterpretationIndex choice={match.params.choice} displayType={"theme"} isEditing={false}/>}
-      render={() => <InterpretationIndex />}
+      render={() => <InterpretationIndex addInterpretation={addInterpretation}/>}
       />
       {/* <Route
       exact path= "/category/title/:choice"
