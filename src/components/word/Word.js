e2f3aa57e13edb0528b7b8ec91c4e92fbe24208f
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-23 18:52:20 -0600

    Fix bug in aync code

--- src/components/word/Word.js
+++ src/components/word/Word.js
@@ -5,51 +5,44 @@ import './Word.css'
 
 const Word = ({word}) => {
   const [definition, setDefinition] = useState('')
-  const [wordComponent, setWordComponent] = useState('')
+
   //this will be used for styling purposes.
   const [hasDefinition, setHasDefinition] = useState(false)
-
   const getDefinition = async () => {
     try {
       const data = await fetchDefinition(word)
       console.log('data---->', data)
       setDefinition(data[0].meanings[0].definitions[0].definition)
+      setHasDefinition(true)
+
     } catch (err) {
       // setError(err)
       console.log(err)
     }
   }
-  
 
-
-  useEffect(async ()=> {
-    console.log("I am in useEFFECT!")
+  useEffect(() => {  
     if (word.length >= 5 ) {
-      console.log("I am in conditional")
-    await getDefinition();
-    setHasDefinition(true);
-    //to do.. .if definition is undefined ... 
-    //  //TO DO: // if(definition ! == undefined)
-    //edgecase that we have an empty tooltip.
-    setWordComponent(
-      <>
-       <a data-tip data-for={word}> {word}: {definition}</a>
-      <ReactTooltip id={word} >
-        <span>{definition}</span>
-      </ReactTooltip>
-      </>
-    )
-    
-    } else {
-      setWordComponent(<span>{word}</span>)
+    getDefinition();
     }
+    
   }, [])
 
-  console.log("wordCompoent", wordComponent)
+
+   
 
   return (
     <div className="Word">
-     {wordComponent}
+      {word.length >= 5 && definition && (
+         <>
+         <a data-tip data-for={word} > {word}: {definition}</a>
+        <ReactTooltip id={word} >
+          <span >{definition}</span>
+        </ReactTooltip>
+        </>
+      )}
+      {word.length < 5 && <span>{word}</span>}
+   
     </div>
   )
 }
