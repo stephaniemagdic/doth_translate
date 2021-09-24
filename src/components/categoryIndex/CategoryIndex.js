bac499abc11f9465ce37a5aae700fcd954ee2dd4
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 16:17:57 -0600

    Set themes and titles to state

--- src/components/categoryIndex/CategoryIndex.js
+++ src/components/categoryIndex/CategoryIndex.js
@@ -4,6 +4,7 @@ import {fetchAllTitles, fetchAllThemes} from '../../util.js'
 export const CategoryIndex = ({category}) => {
   console.log("match-->", category.match.params.type)
   const [allTitles, setAllTitles] = useState([])
+  const [allThemes, setAllThemes] = useState([])
 
   const fetchTitles = async () => {
     try {
@@ -16,16 +17,27 @@ export const CategoryIndex = ({category}) => {
     }
   }
 
+  const fetchThemes = async () => {
+    try {
+      const data = await fetchAllThemes()
+      setAllThemes(data.quote)
+      console.log("data in fetchAllThemes", data)
+    } catch (err) {
+      console.log(err)
+      // setError(err)
+    }
+  }
+
   //Next Step.. pass data to CardsContainer...
 
   useEffect(() => {
     const fetchType = category.match.params.type
     if(fetchType === 'theme' ) {
-      fetchTitles()
+      fetchThemes()
       
     } else if (fetchType === 'title') {
-      //TO DO: COMPLETE
-      return
+      
+      fetchTitles()
     }
   }, [])
 
