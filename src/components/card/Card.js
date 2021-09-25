51db99493d6f10e86d032ea1b1193e856d6eeadb
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 20:43:46 -0600

    Wrap card in button element

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -18,13 +18,11 @@ const Card = ({cardData, type}) => {
     </div>)
   } else if (type === 'theme-options') {
     componentToRender = ( <div className="theme-options">
-      {cardData}
-    <button>CHOOSE</button>
+      <button>{cardData}</button>
     </div>)
   } else if (type === 'title-options') {
     componentToRender = ( <div className="title-options">
-    {cardData}
-  <button>CHOOSE</button>
+    <button>{cardData}</button>
   </div>)
   }
 
