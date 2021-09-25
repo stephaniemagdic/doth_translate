0c72617255e6a6abd5e086911e14fcc0d571f3a6
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-25 14:50:23 -0600

    Add id to quote response object

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,23 +1,23 @@
-Cypress.Commands.add('fetchRandomQuote', () => {
-  cy.intercept('GET', 'https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/single', {
-    statusCode: 200,
-    body: { quote: {
-      id: "266603a8-9d5d-11ea-965d-ac2b6ef23411",
-      play: "(The Comedy of Errors, Act 12, Scene 64)",
-      quote: "But lest myself be guilty to self wrong, i'll stop mine ears against the mermaid's song.",
-      rating: 0
-    }
-  }
-  })
-})
-
+// Cypress.Commands.add('fetchRandomQuote', () => {
+//   cy.intercept('GET', 'https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/single', {
+//     statusCode: 200,
+//     body: { quote: {
+//       id: "266603a8-9d5d-11ea-965d-ac2b6ef23411",
+//       play: "(The Comedy of Errors, Act 12, Scene 64)",
+//       quote: "But lest myself be guilty to self wrong, i'll stop mine ears against the mermaid's song.",
+//       rating: 0
+//     }
+//   }
+//   })
+// })
 
 Cypress.Commands.add('fetchQuoteByTitle', () => {
   cy.intercept('GET', 'https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/by-play/othello', {
     statusCode: 200,
     body: { quote: {
       quote: 
-        'Which thing to do, if this poor trash of Venice, whom trace for his quick hunting, stand the putting on, i\'ll have our Michael Cassio on the hip, abuse him to the Moor in the rank garb (For fear Cassio with my nightcap too), make the Moor thank me, love me, and reward me for making him egregiously an ass and practicing upon his peace and quiet even to madness.'
+        'Which thing to do, if this poor trash of Venice, whom trace for his quick hunting, stand the putting on, i\'ll have our Michael Cassio on the hip, abuse him to the Moor in the rank garb (For fear Cassio with my nightcap too), make the Moor thank me, love me, and reward me for making him egregiously an ass and practicing upon his peace and quiet even to madness.',
+      id: 'test'
     } 
     }
   })
