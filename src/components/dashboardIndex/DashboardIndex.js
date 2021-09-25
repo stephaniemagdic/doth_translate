030a9e06edef8f347b2741631049ba73ed5f62cc
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 20:36:13 -0600

    Add container to buttons for styling

--- src/components/dashboardIndex/DashboardIndex.js
+++ src/components/dashboardIndex/DashboardIndex.js
@@ -1,21 +1,19 @@
-//Welcome page
-//choose a quote category
-//two buttons .. with nav links.
-
 import { Link } from 'react-router-dom';
+import './DashboardIndex.css';
 
 const DashboardIndex = () => {
   return (
     <div className="DashboardIndex">
       <h1>Welcome Lambkin!</h1>
       <h2>Choose a Quote Category</h2>
-      <Link to='/category/theme'>
-      <button className="theme-btn">THEME</button>
-      </Link>
-      <Link to='/category/title'>
-      <button className="title-btn">Title</button>
-      </Link>
-      
+      <div className='button-container'>
+        <Link to='/category/theme'>
+        <button className="theme-btn">Theme</button>
+        </Link>
+        <Link to='/category/title'>
+        <button className="title-btn">Title</button>
+        </Link>
+      </div>
     </div>
   )
 }
