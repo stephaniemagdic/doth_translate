5ec38a8af3082517098aeb60dd28a3acd1e94408
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-25 15:48:30 -0600

    Rid of code comments

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -1,21 +1,11 @@
 import Word from '../word/Word.js';
 import './Quote.css';
 
-
 const Quote = ({quote, addToFavorites, quoteID}) => {
-  console.log("QUOTE-->", quote)
   const words = quote.quote.split(" ").map(word => <Word word={word}/> )
-  console.log(words)
-  console.log("quote id here", quoteID)
-
-
-  
-
-  //am I creating an object where there are definitions for each word that is over 5 letters long
-  // then highlighting the words that have a property of defined on them?
 
   return (
-    <div className="Quote" id={quoteID}>
+    <div className="Quote" id={quoteID} key={quoteID}>
       <div className="words-container">
       {words}
       </div>
@@ -25,7 +15,3 @@ const Quote = ({quote, addToFavorites, quoteID}) => {
 }
 
 export default Quote;
-
-//Love and languish for his sake. B it ounce, or cat, or bear, pard, or boar with bristled hair, in thy eye that shall appear when thou wak'st, it is thy dear.
-
-//(32) ['Love', 'and', 'languish', 'for', 'his', 'sake.', 'B', 'it', 'ounce,', 'or', 'cat,', 'or', 'bear,', 'pard,', 'or', 'boar', 'with', 'bristled', 'hair,', 'in', 'thy', 'eye', 'that', 'shall', 'appear', 'when', 'thou', "wak'st,", 'it', 'is', 'thy', 'dear.']
