bd51b0f3d8b3e639cec04895b7d0e954ee5a109d
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-24 19:38:23 -0600

    Add fetch requests for title and theme

--- src/util.js
+++ src/util.js
@@ -6,13 +6,13 @@ export const fetchDefinition = (word) => {
   return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).then((res) => res.ok ? res.json() : console.log("something went wrong, res:", res)).catch(err => err)
 }
 
-// export const fetchQuoteByTitle = () => {
+export const fetchQuoteByTitle = (title) => {
+  return fetch(`https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/by-play/${title}`).then((res) => res.ok ? res.json() : console.log("something went wrong, res:", res)).catch(err => err)
+}
 
-// }
-
-// export const fetchQuoteByTheme = () => {
-
-// }
+export const fetchQuoteByTheme = (theme) => {
+  return fetch(`https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/by-theme/${theme}`).then((res) => res.ok ? res.json() : console.log("something went wrong, res:", res)).catch(err => err)
+}
 
 export const fetchAllTitles = () => {
   return fetch('https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/play-titles').then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
