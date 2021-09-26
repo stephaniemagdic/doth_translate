6fae48e8b09947d71f1f0fad62aacb93d7dd5d38
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-25 21:32:28 -0600

    Add toggle to disable button on addQuote

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -1,16 +1,23 @@
 import Word from '../word/Word.js';
 import './Quote.css';
+import saveIcon from '../../assets/add.png'
+import {useState} from 'react';
 
 const Quote = ({quote, addToFavorites, quoteID}) => {
+  const [isDisabled, toggleIsDisabled] = useState(false)
   const words = quote.quote.split(" ").map(word => <Word word={word}/> )
-  console.log("quoteeeee", quote)
+
+  const handleClick = () => {
+    addToFavorites(quote.quote, quoteID)
+    toggleIsDisabled(true)
+  }
 
   return (
     <div className="Quote" id={quoteID} key={quoteID}>
       <div className="words-container">
       {words}
       </div>
-      <button onClick={() => addToFavorites(quote.quote, quoteID)} className="favorite-btn" >Save For Later</button>
+      <button onClick={() => handleClick()} className="favorite-btn" disabled={isDisabled}><img src={saveIcon} alt='plus sign' className='saveIcon'></img></button>
     </div>
   )
 }
