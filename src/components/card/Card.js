194ab69a1d356b4d61f917a793bc6d3317123716
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-25 18:18:18 -0600

    Add JSX element tags around text

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,41 +2,40 @@ import './Card.css';
 
 const Card = ({cardData, type}) => {
   let componentToRender;
-
   if (type === 'favorites') {
     componentToRender = (
-      // <div className="favorites" id={cardData.id} key={cardData.id}>
-      <div className="favorites">
+      <div className="favorites" id={cardData.id} key={cardData.id}>
         {cardData.quote}
-        {console.log("favorits card data", cardData)}
       <button>Delete</button>
       <button>Interpret</button>
       </div>
       
     )
-  } else if (type === 'submissions' && cardData.quote) {
-    componentToRender = ( <div className="submissions" >
-      {/* ///saying card data is undefined... */}
-      Quote: {cardData.quote.quote}
-      Your Interpretation: {cardData.interpretation}
-      {console.log("DATATTTTTTT--", cardData)}
-    <button>EDIT</button>
-    </div>)
+  } else if (type === 'submissions' ) {
+    componentToRender = ( 
+      <div className="submissions" >
+        <h3>Quote:</h3> <p> {cardData.quote} </p>
+        <h3>Your Interpretation:</h3> <p> {cardData.interpretation} </p>
+        <button>EDIT</button>
+      </div> 
+    )
   } else if (type === 'theme-options') {
-    componentToRender = ( <div className="theme-options" >
-      <button>{cardData}</button>
-    </div>)
+    componentToRender = ( 
+      <div className="theme-options" >
+        <button>{cardData}</button>
+      </div>
+    )
   } else if (type === 'title-options') {
-    componentToRender = ( <div className="title-options" >
-    <button>{cardData}</button>
-  </div>)
+    componentToRender = (
+      <div className="title-options" >
+        <button>{cardData}</button>
+      </div>
+    )
   }
-
   return (
     <div>
       {componentToRender}
     </div>
-   
   )
 }
 
