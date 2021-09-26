cbfd4f1814b6fa2cda8562930d6f654cadf3e100
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-25 19:58:08 -0600

    Fix how to access quote data

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -3,13 +3,14 @@ import './Quote.css';
 
 const Quote = ({quote, addToFavorites, quoteID}) => {
   const words = quote.quote.split(" ").map(word => <Word word={word}/> )
+  console.log("quoteeeee", quote)
 
   return (
     <div className="Quote" id={quoteID} key={quoteID}>
       <div className="words-container">
       {words}
       </div>
-      <button onClick={() => addToFavorites(quote, quoteID)} className="favorite-btn" >Save For Later</button>
+      <button onClick={() => addToFavorites(quote.quote, quoteID)} className="favorite-btn" >Save For Later</button>
     </div>
   )
 }
