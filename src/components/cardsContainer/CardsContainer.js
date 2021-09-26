60977d79e268ebacc03b7814d83fa55697cab31c
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-25 18:37:42 -0600

    Clean up code spacing

--- src/components/cardsContainer/CardsContainer.js
+++ src/components/cardsContainer/CardsContainer.js
@@ -4,57 +4,52 @@ import './CardsContainer.css';
 
 const CardsContainer = ({cards, type}) => {
    let cardComponents;
-
-   if(type === "theme-options" ) {
-     
-   cardComponents = cards.map(card =>
-      <div key={card}>
-       <Link to={`/category/theme/${card}`}>
-      <Card cardData={card} type={type}/>
-      </Link>
-      </div>
-      )
-   } else if (type === "title-options") {
-
-
-       cardComponents = cards.map(card =>
+   if (type === 'theme-options' ) {
+      cardComponents = cards.map(card =>
          <div key={card}>
-       <Link to={`/category/title/${card}`}>
-      <Card cardData={card} type={type}/>
-      </Link>
-      </div>
+            <Link to={`/category/theme/${card}`}>
+               <Card cardData={card} type={type}/>
+            </Link>
+         </div>
       )
-   } else if (type === "submissions" || type === "favorites") {
-       cardComponents = cards.map(card => <Card cardData={card} type={type}/>)
+   } else if (type === 'title-options') {
+      cardComponents = cards.map(card =>
+         <div key={card}>
+            <Link to={`/category/title/${card}`}>
+               <Card cardData={card} type={type}/>
+            </Link>
+         </div>
+      )
+   } else if (type === 'submissions' || type === 'favorites') {
+      cardComponents = cards.map(card => <Card cardData={card} type={type}/>)
    }
-
-   
 
     return (
       <>
-      { type === "submissions" && (<div className="interpretations">
-      <h2>My Interpretations</h2>
-      {cardComponents}
-   </div>)}
-      
-   { type === "favorites" && (<div className="favorites">
-      <h2>Saved Quotes</h2>
-      {cardComponents}
-   </div>)}
-
-   { type === "title-options" && (<div className="titles">
-      {cardComponents}
-   </div>)}
-
-   { type === "theme-options" && (<div className="themes">
-      {cardComponents}
-   </div>)}
-
-      
+      { type === 'submissions' && (
+         <div className="interpretations">
+            <h2>My Interpretations</h2>
+            {cardComponents}
+         </div>
+         )}   
+      { type === 'favorites' && (
+         <div className="favorites">
+            <h2>Saved Quotes</h2>
+            {cardComponents}
+         </div>
+         )}
+      { type === 'title-options' && (
+         <div className="titles">
+            {cardComponents}
+         </div>
+         )}
+      { type === "theme-options" && (
+         <div className="themes">
+            {cardComponents}
+         </div>
+         )}
       </>
-     
     )
-
 }
 
 export default CardsContainer;
