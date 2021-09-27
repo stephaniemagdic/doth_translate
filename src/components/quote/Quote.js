3389e6119b75df6a5f2b5826d23e65d4dc557f8e
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-26 19:45:38 -0600

    Import UUID to create unique id's

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -1,17 +1,28 @@
 import Word from '../word/Word.js';
 import './Quote.css';
 import saveIcon from '../../assets/add.png'
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import uuid from 'react-uuid';
 
 const Quote = ({quote, addToFavorites, quoteID}) => {
   const [isDisabled, toggleIsDisabled] = useState(false)
-  const words = quote.quote.split(" ").map(word => <Word word={word}/> )
+  const [isLoading, setIsLoading] = useState(true)
+  const [count, setCount] = useState(0)
+
+  const countDefinitionsFetched = () => {
+    // setCount(count + 1)
+  }
+
+  const words = quote.quote.split(" ").map(word => <Word word={word} countDef={countDefinitionsFetched} id={word} key={uuid()}/> )
 
   const handleClick = () => {
     addToFavorites(quote.quote, quoteID)
     toggleIsDisabled(true)
   }
 
+ 
+
+ 
   return (
     <div className="Quote" id={quoteID} key={quoteID}>
       <div className="words-container">
