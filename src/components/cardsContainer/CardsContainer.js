08df7e9812ddcd62741bc09c9aba3eb341dca981
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-27 14:13:58 -0600

    Rename methods for clarity

--- src/components/cardsContainer/CardsContainer.js
+++ src/components/cardsContainer/CardsContainer.js
@@ -1,11 +1,11 @@
-import Card from '../card/Card';
 import {Link} from 'react-router-dom';
 import './CardsContainer.css';
+import Card from '../card/Card';
 import uuid from 'react-uuid';
 
-const CardsContainer = ({cards, type, deleteQuoteFromStorage}) => {
+const CardsContainer = ({cards, type, deleteQuote}) => {
    let cardComponents;
-   if (type === 'theme-options' ) {
+   if (type === 'theme-options') {
       cardComponents = cards.map(card =>
          <div key={card}>
             <Link to={`/category/theme/${card}`}>
@@ -22,36 +22,37 @@ const CardsContainer = ({cards, type, deleteQuoteFromStorage}) => {
          </div>
       )
    } else if (type === 'submissions' || type === 'favorites') {
-      cardComponents = cards.map(card => <Card cardData={card} type={type} deleteQuoteFromStorage={deleteQuoteFromStorage} key={uuid()}/>)
+      cardComponents = cards.map(card => <Card cardData={card} type={type} deleteQuote={deleteQuote} key={uuid()} />)
    }
 
     return (
-      <>
-      { type === 'submissions' && (
-         <div className="interpretations">
-            <h2 className='title'>My Interpretations</h2>
-            {cardComponents}
-            {!cardComponents.length && <button><Link to="/">Go back to start interpreting</Link></button>}
-         </div>
+      <div className='CardsContainer'>
+         {type === 'submissions' && (
+            <div className="interpretations">
+               <h2 className='title'>My Interpretations</h2>
+               {cardComponents}
+               {!cardComponents.length && <button><Link to="/">Go back to start interpreting</Link></button>}
+            </div>
          )}   
-      { type === 'favorites' && (
-         <div className="favorites">
-            <h2 className='title'>Saved Quotes</h2>
-            <div>{cardComponents}</div> 
-            {!cardComponents.length && <button><Link to="/">Find a favorite Quote</Link></button>}
-         </div>
+         {type === 'favorites' && (
+            <div className="favorites">
+               <h2 className='title'>Saved Quotes</h2>
+               {cardComponents} 
+               {!cardComponents.length && (
+               <button><Link to="/">Find a favorite Quote</Link></button>)}
+            </div>
          )}
-      { type === 'title-options' && (
-         <div className="titles">
-            {cardComponents}
-         </div>
+         {type === 'title-options' && (
+            <div className="titles">
+               {cardComponents}
+            </div>
          )}
-      { type === "theme-options" && (
-         <div className="themes">
-            {cardComponents}
-         </div>
+         {type === "theme-options" && (
+            <div className="themes">
+               {cardComponents}
+            </div>
          )}
-      </>
+      </div>
     )
 }
 
